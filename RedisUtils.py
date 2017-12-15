d208b8a6388d63efff357578aff28cd8c27eb679
Pavel Rybintsev <pavelryb86@gmail.com>
2017-12-15 23:12:01 +0200

    converting measurment to json correctly

--- RedisUtils.py
+++ RedisUtils.py
@@ -6,3 +6,7 @@ from Config import REDIS_HOST, REDIS_PORT
 def get_redis_connection():
     pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
     return redis.Redis(connection_pool=pool)
+
+
+def convert_keys_and_values_to_dict(keys, values):
+    return {key: value for key, value in zip(keys, values)}
--- Web/Urls.py
+++ Web/Urls.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 
 from Config import LAST_MEASUREMENT_KEY, TIME_KEY, TEMPERATURE_KEY, HUMIDITY_KEY
-from RedisUtils import get_redis_connection
+from RedisUtils import get_redis_connection, convert_keys_and_values_to_dict
 
 APP_NAME = 'baby_sensor'
 app = Flask(APP_NAME)
@@ -11,8 +11,10 @@ app = Flask(APP_NAME)
 def last_measurement():
     redis_conn = get_redis_connection()
     last_measurement_key = redis_conn.get(LAST_MEASUREMENT_KEY)
+    keys = [TIME_KEY, TEMPERATURE_KEY, HUMIDITY_KEY]
     measurement = redis_conn.hmget(last_measurement_key, [TIME_KEY, TEMPERATURE_KEY, HUMIDITY_KEY])
-    return jsonify(measurement)
+    
+    return jsonify(convert_keys_and_values_to_dict(keys, measurement))
 
 
 if __name__ == "__main__":
