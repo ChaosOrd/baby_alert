b778c022c4f2ee424475e8a242a133315b873950
Pavel Rybintsev <pavelryb86@gmail.com>
2017-12-20 23:05:22 +0200

    local time fix

--- Web/static/data_loader.js
+++ Web/static/data_loader.js
@@ -11,7 +11,7 @@ function drawCurveTypes(data, status) {
     graphData.addColumn('number', 'Humidity');
 
     $.each(data.measurements, function(idx, measurement) {
-        var measurementTime = new Date(measurement.time);
+        var measurementTime = new Date(measurement.time + date.getTimezoneOffset()*60*1000);
         graphData.addRow([measurementTime, Number(measurement.temperature), Number(measurement.humidity)]);
     });
 
